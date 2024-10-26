d23a133b5c647f0f0674e1cccdf583945473c685
Shailaja-2 <167770048+Shailaja-2@users.noreply.github.com>
2024-10-26 21:56:04 +0530

    commit

---
+++ React/sample-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+
+const App = () => {
+    const data={
+        appname:"MyApp"
+    }
+    const logodata="mylogo"
+    const test="HelloWorld\t"
+    const Myname="shailu\n\n\n"
+
+  return (
+    
+    <>
+        {Myname}
+        <p className="text-red-700">{test}</p>
+        <Navbar appdata={data} logo={logodata} />
+    </>
+  )
+
+}
+export default App
