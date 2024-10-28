b208504d3bfab2711a307ad4520d748ce60555c0
Shailaja-2 <167770048+Shailaja-2@users.noreply.github.com>
2024-10-28 14:21:54 +0530

    commit

--- React/route-project/src/App.jsx
+++ React/route-project/src/App.jsx
@@ -3,16 +3,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Product from './pages/Product'
 import Contact from './pages/Contact'
+import WebLayout from './layout/WebLayout'
 
 const App = () => {
   return (
     <>
         <BrowserRouter>
             <Routes>
+              <Route element={<WebLayout />}>
                 <Route path='/' element={<Home />}/>
                 <Route path='/products' element={<Product />}/>
                 <Route path='/contacts' element={<Contact />}/>
-                
+              </Route>  
             </Routes>
         </BrowserRouter>
     </>
--- React/route-project/src/components/Navbar.jsx
+++ React/route-project/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
 
@@ -9,25 +10,32 @@ const Navbar = () => {
     },
     {
       title: 'Product',
-      path: '/'
+      path: '/products'
     },
     {
       title: 'Contact',
-      path: '/'
-    },
-      ]
+      path: '/contacts'
+    }
+  ]
   return (
+
     <>
+
        <div className='w-screen h-14 shadow-purple-500 shadow-md flex flex-row justify-center items-center'>
           <div className='w-[40%] flex justify-start items-center font-bold text-2x1 text-purple-500 '>
             Max Store
           </div>
-            <div className='w-[40%] flex justify-end items-center'>
-              <div className='w-full flex flex-row justify-end items-center gap-8 font-semibold'>
+            <div className='w-[40%] h-full flex justify-end items-center'>
+              <div className='w-full h-full flex flex-row justify-end items-center gap-8 font-bold'>
                 {/* <li>Home</li>
                 <li>Product</li>
                 <li>Contact</li> */}
-                
+                  {Linksdata.map((link, index) => (
+                    <NavLink to={link.path} key={index} className='h-[65%] w-20 hover:bg-purple-500 hover:text-white flex justify-center items-center rounded-sm'>
+                      {link.title}
+                    </NavLink>
+                  ))
+                  }
               </div>
             </div>
        </div>
