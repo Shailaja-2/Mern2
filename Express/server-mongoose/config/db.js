6e2eeeb9d83718368f1cd11808fda3f238fcc9dd
Shailaja-2 <167770048+Shailaja-2@users.noreply.github.com>
2024-10-29 20:47:08 +0530

    D7

--- Express/server-mongoose/config/db.js
+++ Express/server-mongoose/config/db.js
@@ -4,7 +4,10 @@ mongoose.connect('mongodb+srv://SHAILAJA:shailaja%4012345@in-aws.hfan5.mongodb.n
 
 const connection=mongoose.connection;
 
+
+
 connection.on('connected', () => (console.log("DB Connected")))
 connection.on('error', () => (console.log("DB Error")))
+
 
 module.exports = mongoose
